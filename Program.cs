42a6303df04e9634f9ff94d630d2c8e83fa243be
Muhammad Bilal Raza <br6918322@gmail.com>
2026-04-03 22:53:16 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MidDB26_2025CS10.UI;
+
+namespace MidDB26_2025CS10
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginForm());
+            // Finalized for Mid-Term Submission
+        }
+
+    }
+}
